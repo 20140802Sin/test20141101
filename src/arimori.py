ea0a36567f49d9376c3c5935c58c94098db8137b
main <main@home3>
2014-11-01 09:42:05 +0900

    追加：arimoriでIwasGay

---
+++ src/arimori.py
@@ -0,0 +1,14 @@
+'''
+Created on 2014/11/01
+
+@author: main
+'''
+def arimori():
+
+    print "I was gay. "
+
+    return
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    arimori()
